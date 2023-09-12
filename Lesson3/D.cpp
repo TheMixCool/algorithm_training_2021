4511585b1711f1b972e68cbce0309e327c9e62f2
Egor <mixcraftcool@icloud.com>
2023-09-12 20:39:37 +0300

    initial commit

---
+++ Lesson3/D.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <set>
+#include <string>
+
+int main() {
+	std::set<std::string> str;
+	std::string temp;
+
+	while (std::cin >> temp) {
+		str.emplace(temp);
+	}
+	std::cout << str.size();
+}
