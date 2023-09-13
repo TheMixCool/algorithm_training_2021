dc4c727847e5bbc43487a7f619ea1a37ac114234
Egor <mixcraftcool@icloud.com>
2023-09-14 01:48:27 +0300

    initial commit

---
+++ Lesson4/Lecture/count_sort.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+	int a, b;
+	std::cin >> a >> b;
+
+	std::vector<int> arrA(10), arrB(10);
+
+	while (a > 0) {
+		arrA.at(a % 10)++;
+		a /= 10;
+	}
+
+	while (b > 0) {
+		arrB.at(b % 10)++;
+		b /= 10;
+	}
+
+	for (int i = 0; i != 10; ++i) {
+		if (arrA.at(i) != arrB.at(i)) {
+			std::cout << "NO" << std::endl;
+			return 0;
+		}
+	}
+	std::cout << "YES" << std::endl;
+	return 0;
+
+}
