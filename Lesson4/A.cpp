dd5b28ef1be47e5783642f0d9ed59d4ce9cb2a12
Egor <mixcraftcool@icloud.com>
2023-09-15 01:10:21 +0300

    initial commit

---
+++ Lesson4/A.cpp
@@ -0,0 +1,26 @@
+﻿#include <string>
+#include <map>
+#include <iostream>
+
+int main() {
+	std::string tempKey, tempValue;
+	std::map<std::string, std::string> mp;
+	std::map<std::string, std::string> mp2;
+
+	int count = 0;
+	std::cin >> count;
+
+	for (int i = 0; i != count; ++i) {
+		std::cin >> tempKey >> tempValue;
+		mp[tempKey] = tempValue;
+		mp2[tempValue] = tempKey;
+	}
+
+	std::cin >> tempKey;
+
+	if (mp.find(tempKey) != mp.end())
+		std::cout << mp[tempKey];
+	else
+		std::cout << mp2[tempKey];
+	return 0;
+}
