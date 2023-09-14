d4afd8565d715f3357b9f96770b6ed6951935ecf
Egor <mixcraftcool@icloud.com>
2023-09-15 00:35:45 +0300

    initial commit

---
+++ Lesson4/Lecture/gistogramma.cpp
@@ -0,0 +1,31 @@
+﻿#include <map>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string str = "Hello, world!";
+	std::map<char, int> mp;
+	for (auto& ch : str)
+		mp[ch]++;
+
+	int max = 0;
+	for (auto& [ch, cn] : mp) {
+		if (cn > max) 
+			max = cn;
+	}
+	int temp = max;
+	for (int i = 0; i != max; ++i) {
+		for (auto& [ch, cn] : mp) {
+			if (cn >= temp) 
+				std::cout << '#';
+			else 
+				std::cout << ' ';
+		}
+		temp--;
+		std::cout << std::endl;
+	}
+	for (auto& [ch, cn] : mp)
+		std::cout << ch;
+	
+	return 0;
+}
