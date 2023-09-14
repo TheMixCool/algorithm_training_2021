8a2541b7c17135244bdf0d1553061222b013b3c3
Egor <mixcraftcool@icloud.com>
2023-09-14 17:01:46 +0300

    initial commit

---
+++ Lesson4/Lecture/chess_ex.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <map>
+#include <utility>
+
+int GetCount(std::map<std::pair<int, int>, int>& mp) {
+	int count = 0;
+
+	std::map<int, int> mp1;
+	std::map<int, int> mp2;
+
+	for (auto& [f, s] : mp) {
+		mp1[f.first]++;
+	}
+	for (auto& [f, s] : mp) {
+		mp2[f.second]++;
+	}
+	return mp1.size() + mp2.size() - 2;
+}
+
+
+int main() {
+	std::map<std::pair<int, int>, int> mp;
+	
+	mp[std::pair<int, int>(1, 1)]++;
+	mp[std::pair<int, int>(1, 2)]++;
+	mp[std::pair<int, int>(1, 3)]++;
+	mp[std::pair<int, int>(2, 2)]++;
+
+	std::cout << GetCount(mp);
+
+}
