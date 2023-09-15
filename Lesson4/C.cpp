0d61813fbf19a5bfbe5015342d28ba67b021d9f1
Egor <mixcraftcool@icloud.com>
2023-09-15 03:17:33 +0300

    initial commit

---
+++ Lesson4/C.cpp
@@ -0,0 +1,24 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+int main() {
+	std::map<std::string, int> mp;
+
+	std::string temp;
+	int max = 0;
+	std::string res;
+
+	while (std::cin >> temp) {
+		mp[temp]++;
+	}
+	for (auto& [k, v] : mp) {
+		if (v > max) {
+			max = v;
+			res = k;
+		}
+	}
+
+	std::cout << res;
+	return 0;
+}
