13dba83bdebf45bfe4a78b629a43348ff7d6ab0a
Egor <mixcraftcool@icloud.com>
2023-09-15 03:11:01 +0300

    initial commit

---
+++ Lesson4/B.cpp
@@ -0,0 +1,20 @@
+﻿#include <map>
+#include <string>
+#include <iostream>
+
+int main() {
+	std::map<std::string, int> mp;
+
+	std::string temp;
+	
+	while (std::cin >> temp) {
+		if (mp.find(temp) == mp.end()) {
+			std::cout << 0 << ' ';
+			mp[temp]++;
+		}
+		else {
+			std::cout << mp[temp]++ << ' ';
+		}
+	}
+	return 0;
+}
