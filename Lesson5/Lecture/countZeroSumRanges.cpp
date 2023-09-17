6cc40baa6120418a0bf8944d78a9382b4afc07cd
Egor <mixcraftcool@icloud.com>
2023-09-17 22:16:05 +0300

    initial commit

---
+++ Lesson5/Lecture/countZeroSumRanges.cpp
@@ -0,0 +1,36 @@
+﻿#include <vector>
+#include <map>
+#include <iostream>
+
+std::map<int,int> MakePrefix(std::vector<int>& v) {
+	std::map<int, int> mp {{0, 1}};
+	int currentSum = 0;
+
+	for (auto& ch : v) {
+		currentSum += ch;
+		if (mp.find(currentSum) != mp.end()) {
+			mp[currentSum] += 1;
+		}
+		else {
+			mp[currentSum] = 0;
+		}
+	}
+	return mp;
+}
+
+int CountZeroSumRanges(std::map<int, int> &mp) {
+	int countRanges = 0;
+	
+	for (auto& [k, v] : mp) {
+		int cntSum = v;
+		countRanges += cntSum * (cntSum - 1) / 2;
+	}
+	return countRanges;
+}
+
+int main() {
+	std::vector<int> values {1, 3, 5, -5, -3, -1, 0, 1, 3 ,-3};
+	std::map<int, int> prefix(MakePrefix(values));
+	
+	std::cout << CountZeroSumRanges(prefix);
+}
