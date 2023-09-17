625f21c3ecfef73572f490169632702d4255d130
Egor <mixcraftcool@icloud.com>
2023-09-17 23:01:13 +0300

    initial commit

---
+++ Lesson5/Lecture/pairCount.cpp
@@ -0,0 +1,25 @@
+﻿#include <vector>
+#include <iostream>
+
+int PairCount(std::vector<int>& v, int k) {
+	int res = 0;
+	int l = 0;
+	int r = l + 1;
+	
+	while (r != v.size()) {
+		if (v[r] - v[l] <= k) {
+			r++;
+		}
+		else if (v[r] - v[l] > k) {
+			res += v.size() - r;
+			l++;
+		}
+	}
+	return res;
+}
+
+int main() {
+	std::vector<int> values {1, 3,6,7,8 };
+	
+	std::cout << PairCount(values, 4);
+}
