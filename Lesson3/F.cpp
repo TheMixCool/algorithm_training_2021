fc37ea8b4745f0695cc8c33b577ff638812939b8
Egor <mixcraftcool@icloud.com>
2023-09-19 00:35:57 +0300

    initial commit

---
+++ Lesson3/F.cpp
@@ -0,0 +1,23 @@
+﻿#include <set>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string str1, str2;
+	std::cin >> str1 >> str2;
+
+	std::set<std::string> st;
+
+	for (int i = 0; i != str2.size() - 1; ++i) {
+		st.emplace(str2.substr(i, 2));
+	}
+	int res = 0;
+
+	for (int i = 0; i != str1.size()-1; ++i) {
+		std::string tmp = str1.substr(i, 2);
+		if (st.find(tmp) != st.end()) {
+			res++;
+		}
+	}
+	std::cout << res;
+}
